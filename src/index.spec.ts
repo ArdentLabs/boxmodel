165a75858206d409a69dc37422b980e2e8aa6484
Sam Balana <me@sambalana.com>
2017-08-29 14:28:19 -0700

    Initial commit for tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,9 +1,48 @@
-import { equal } from 'assert'
+import { equal, ok } from 'assert'
+import generate from './index'
+import { InputOptions } from '../index'
+import { schema } from 'normalizr'
 
-describe('generate function', () => {
-  it('should work', () => {
-    const expected: string = 'bye'
-    const actual: string = 'hi'
-    equal(actual, expected)
+describe('generate()', () => {
+  const options: InputOptions = {
+    modelName: 'car',
+    schema: new schema.Entity('cars'),
+    apiUrl: 'localhost:3000',
+    fields: `
+      make
+      model
+    `,
+  }
+
+  const res = generate(options)
+
+  it('has $$isBoxModel', () => {
+    ok(res.$$isBoxModel)
   })
+  it('has actions', () => {
+    ok(res.actions)
+  })
+  it('has paths', () => {
+    ok(res.paths)
+  })
+  it('has reducer', () => {
+    ok(res.reducer)
+  })
+  it('has routes', () => {
+    ok(res.routes)
+  })
+  it('has sagas', () => {
+    ok(res.sagas)
+  })
+  it('has selectors', () => {
+    ok(res.selectors)
+  })
+  it('has types', () => {
+    ok(res.types)
+  })
+
+  it('should pluralize modelName', () => {
+    equal(res.pluralModelName, 'cars')
+  })
+
 })
--- src/types.spec.ts
+++ src/types.spec.ts
@@ -1,17 +1,50 @@
-// import { equal } from 'assert'
-//
-// import { generateTypes } from './types'
+import { equal } from 'assert'
+import { schema } from 'normalizr'
 
-describe('type factory', () => {
-  // it('should work', () => {
-  //   const experimentTypes = generateTypes('experiment')
-  //
-  //   it('should generate correct types', () => {
-  //     equal(experimentTypes.get, ['EXPERIMENT_GET', 'EXPERIMENT_GET_OK', 'EXPERIMENT_GET_FAIL'])
-  //     equal(experimentTypes.create, ['EXPERIMENT_CREATE', 'EXPERIMENT_CREATE_OK', 'EXPERIMENT_CREATE_FAIL'])
-  //     equal(experimentTypes.fetch, ['EXPERIMENT_FETCH', 'EXPERIMENT_FETCH_OK', 'EXPERIMENT_FETCH_FAIL'])
-  //     equal(experimentTypes.update, ['EXPERIMENT_UPDATE', 'EXPERIMENT_UPDATE_OK', 'EXPERIMENT_UPDATE_FAIL'])
-  //     equal(experimentTypes.archive, ['EXPERIMENT_ARCHIVE', 'EXPERIMENT_ARCHIVE_OK', 'EXPERIMENT_ARCHIVE_FAIL'])
-  //   })
-  // })
+import { generateTypes, getRequest, getOk, getFail } from './types'
+import { Options } from '../index'
+
+describe('generateTypes()', () => {
+  const options: Options = {
+    modelName: 'car',
+    pluralModelName: 'cars',
+    schema: new schema.Entity('cars'),
+    apiUrl: 'localhost:3000',
+    fields: `
+      make
+      model
+    `,
+  }
+
+  const types = generateTypes(options)
+
+  describe('GET types', () => {
+    it('should generate request type', () => equal(getRequest(types.get), 'CAR_GET'))
+    it('should generate ok type', () => equal(getOk(types.get), 'CAR_GET_OK'))
+    it('should generate fail type', () => equal(getFail(types.get), 'CAR_GET_FAIL'))
+  })
+
+  describe('FETCH types', () => {
+    it('should generate request type', () => equal(getRequest(types.fetch), 'CAR_FETCH'))
+    it('should generate ok type', () => equal(getOk(types.fetch), 'CAR_FETCH_OK'))
+    it('should generate fail type', () => equal(getFail(types.fetch), 'CAR_FETCH_FAIL'))
+  })
+
+  describe('CREATE types', () => {
+    it('should generate request type', () => equal(getRequest(types.create), 'CAR_CREATE'))
+    it('should generate ok type', () => equal(getOk(types.create), 'CAR_CREATE_OK'))
+    it('should generate fail type', () => equal(getFail(types.create), 'CAR_CREATE_FAIL'))
+  })
+
+  describe('UPDATE types', () => {
+    it('should generate request type', () => equal(getRequest(types.update), 'CAR_UPDATE'))
+    it('should generate ok type', () => equal(getOk(types.update), 'CAR_UPDATE_OK'))
+    it('should generate fail type', () => equal(getFail(types.update), 'CAR_UPDATE_FAIL'))
+  })
+
+  describe('ARCHIVE types', () => {
+    it('should generate request type', () => equal(getRequest(types.archive), 'CAR_ARCHIVE'))
+    it('should generate ok type', () => equal(getOk(types.archive), 'CAR_ARCHIVE_OK'))
+    it('should generate fail type', () => equal(getFail(types.archive), 'CAR_ARCHIVE_FAIL'))
+  })
 })
